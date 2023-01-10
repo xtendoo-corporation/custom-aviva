08521466bc8f26958b0b23f288e2cc29e724f04c
dani.xtendoo <dani.xtendoo@gmail.com>
2023-01-10 10:44:47 +0100

    [ADD] aviva_document_format

---
+++ aviva_document_format/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Aviva Document Format",
+    "summary": """Formatos de impresión de AVIVA""",
+    "version": "16.0.1.0.1",
+    "description": """Formatos de impresión de AVIVA""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/layout/external_layout_bold.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
